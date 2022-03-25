bdf7aa48e6d1642310e468dc274dda9cbbe7cfa5
firedrak <101325463+firedrak@users.noreply.github.com>
2022-03-25 10:34:38 +0530

    Update listener.py

--- listener.py
+++ listener.py
@@ -19,16 +19,16 @@ redis_port = 6379
 REDIS_CLI = redis.StrictRedis(
     host=redis_host, port=redis_port, decode_responses=True)
 
-def get_spider(self):
+def get_spider():
     return self.REDIS_CLI.rpop('spiders')
 
-def get_active_process(self, host):
+def get_active_process(host):
     return self.REDIS_CLI.get(f'active_process_of_{host}')
 
-def inc_active_process(self, host):
+def inc_active_process(host):
     self.REDIS_CLI.incr(f'active_process_of_{host}')
 
-def decr_active_process(self, host):
+def decr_active_process(host):
     self.REDIS_CLI.decr(f'active_process_of_{host}')
 
 def start_executor(redis_host, spider_url):
