f02cb40af321e2eac65ff18bd8d9c8d577aab7f0
firedrak <101325463+firedrak@users.noreply.github.com>
2022-03-25 17:16:47 +0530

    Update listener.py

--- listener.py
+++ listener.py
@@ -38,7 +38,7 @@ def decr_active_process(listener_name):
 
 def start_executor(redis_host, spider_url):
 #     subprocess.call(["time", "python3", "crawler/main.py", redis_host, spider_url], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-    subprocess.call(["time", "python3", "crawler/main.py", redis_host, spider_url])
+    subprocess.call(["python3", "crawler/main.py", redis_host, spider_url])
     decr_active_process(listener_name)
     print('Crawling finished ', f'active process : {get_active_process(listener_name)}')
 
