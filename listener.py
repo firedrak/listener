77893969ad4463d03fb3c369777f846b16989b71
firedrak <101325463+firedrak@users.noreply.github.com>
2022-03-25 11:20:26 +0530

    Update listener.py

--- listener.py
+++ listener.py
@@ -35,7 +35,8 @@ def decr_active_process(listener_name):
     REDIS_CLI.decr(f'active_process_of_{listener_name}')
 
 def start_executor(redis_host, spider_url):
-    subprocess.call(["bash", "shell/shell.sh", f"{redis_host} {spider_url}"])
+    subprocess.call(["bash", "shell/shell.sh"])
+    subprocess.call(["time", "python3", "crawler/main.py", f"{redis_host} {spider_url}"])
     decr_active_process(listener_name)
 
 subprocess.check_output(["sudo", "rm", "-rf", "shell"])
